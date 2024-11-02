794e05bfca82e4ea3b615a26a5cdf365f6e0e247
Andreas Christiansen <andycchhrr@gmail.com>
2024-11-02 11:24:59 +0100

    remove Navbar and About/Contact pages; simplify routing in App component

--- client/src/views/contact/Contact.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-    return (
-        <div className="max-w-6xl mx-auto px-4 py-10">
-        <h1 className="text-4xl font-bold text-center">Contact</h1>
-        <p className="text-center mt-5">This is the Contact page.</p>
-        </div>
-    );
-    }
